d463d714707ece01d7d75553de607ffa1424cf08
kanokgan@gmail.com <kanokgan@gmail.com>
2023-09-01 16:41:58 +0700

    Two sum

--- src/utils/codeinterview.ts
+++ src/utils/codeinterview.ts
@@ -1,4 +1,34 @@
 export const TwoSum = (nums: number[], target: number): number[] => {
-    return [];
+  const hashTable = new Map();
+  for (var i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (hashTable.has(diff)) {
+      return [hashTable.get(diff), i];
+    }
+    hashTable.set(nums[i], i);
+  }
+  return [];
 };
 
+export const ValidParentheses = (s: string): boolean => {
+    if (s == null || s.length < 2 || s.length % 2 == 1) return false;
+
+    const dict = new Map();
+    dict.set('}', '{');
+    dict.set(')', '(');
+    dict.set(']', '[');
+  
+    let openBracket: string[] = []
+  
+    for (const c of s) {
+      if (['(', '[', '{'].includes(c)) {
+        openBracket.push(c);
+      } else if (openBracket.pop() !== dict.get(c)){
+          return false;
+      }
+    }
+  
+    if (openBracket.length != 0) return false;
+    
+    return true;
+};
