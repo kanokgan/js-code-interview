502ee5f12b5c82e88217cd0086d7bf9cfcbcb197
kanokgan@gmail.com <kanokgan@gmail.com>
2023-09-05 10:28:10 +0700

    Disable github copilot on this project

--- src/utils/codeinterview.ts
+++ src/utils/codeinterview.ts
@@ -32,3 +32,24 @@ export const ValidParentheses = (s: string): boolean => {
 
   return true;
 };
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+class ListNode {
+  val: number
+  next: ListNode | nuill
+  constructor(val?: number, next?: ListNode | null) {
+}
+export const mergeTwoLists = (
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null => {};
